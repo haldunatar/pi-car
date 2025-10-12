abf717442b83ae3c366691b7eb10106b96f5e1ad
Haldun <atarhaldun@gmail.com>
2025-10-12 10:00:41 +0200

    use plain js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,176 +1,164 @@
-$(function() {
+document.addEventListener("DOMContentLoaded", () => {
+  const browser = navigator.userAgent;
+  const isChrome = browser.includes("Chrome");
 
-    var browser = navigator.userAgent;
-    var isChrome = browser.indexOf('Chrome') > -1;
+  // Command Settings
+  let engineStart = false;
+  const leftButton = document.querySelector(".direction-button--left");
+  const rightButton = document.querySelector(".direction-button--right");
+  const forwardButton = document.querySelector(".direction-button--forward");
+  const backButton = document.querySelector(".direction-button--back");
+  const engineButton = document.querySelector(".engine-button");
+  const voiceControl = document.querySelector("#voiceCommand");
+  voiceControl.style.display = "none";
+  const pressed = "direction-button--pressed";
 
-    // Command Settings
-    var engineStart = false;
-    var leftButton = $('.direction-button--left');
-    var rightButton = $('.direction-button--right');
-    var forwardButton = $('.direction-button--forward');
-    var backButton = $('.direction-button--back');
-    var engineButton = $('.engine-button');
-    var voiceControl = $('#voiceCommand').hide();
-    var pressed = 'direction-button--pressed';
+  if (isChrome) {
+    initializeVoiceControl();
+  }
 
-    if(isChrome) {
-        initializeVoiceControl();
+  engineButton.addEventListener("click", () => {
+    if (engineStart) {
+      engineStart = false;
+      engineActive(false);
+      engineButton.textContent = "Start";
+      engineButton.classList.remove("engine-button--stop");
+      document
+        .querySelectorAll(".direction-button")
+        .forEach((btn) => btn.classList.remove("direction-button--on"));
+      voiceControl.style.display = "none";
+    } else {
+      engineStart = true;
+      engineActive(true);
+      engineButton.textContent = "Stop";
+      engineButton.classList.add("engine-button--stop");
+      document
+        .querySelectorAll(".direction-button")
+        .forEach((btn) => btn.classList.add("direction-button--on"));
+      if (isChrome) {
+        voiceControl.style.display = "block";
+      }
     }
+  });
 
-    $(engineButton).click(function() {
+  // Joystick
+  const addDirectionEvents = (button, direction) => {
+    button.addEventListener("mouseup", () => {
+      callDirection("stop");
+      button.classList.remove(pressed);
+    });
+    button.addEventListener("touchend", () => {
+      callDirection("stop");
+      button.classList.remove(pressed);
+    });
+    button.addEventListener("mousedown", () => {
+      callDirection(direction);
+      button.classList.add(pressed);
+    });
+    button.addEventListener("touchstart", () => {
+      callDirection(direction);
+      button.classList.add(pressed);
+    });
+  };
 
-        if(engineStart) {
-            engineStart = false;
-            engineActive(false);
-            $(this).text('Start');
-            $(this).removeClass('engine-button--stop');
-            $('.direction-button').removeClass('direction-button--on');
-            $(voiceControl).hide();
-        } else {
-            engineStart = true;
-            engineActive(true);
-            $(this).text('Stop');
-            $(this).addClass('engine-button--stop');
-            $('.direction-button').addClass('direction-button--on');
+  addDirectionEvents(leftButton, "left");
+  addDirectionEvents(rightButton, "right");
+  addDirectionEvents(forwardButton, "forward");
+  addDirectionEvents(backButton, "back");
 
-            if(isChrome) {
-                $(voiceControl).show();
-            }
-        }
+  // Keyboard commands
+  document.addEventListener("keydown", keyDown);
+  document.addEventListener("keyup", keyUp);
+
+  function keyDown(e) {
+    switch (e.keyCode) {
+      case 37:
+        callDirection("left");
+        break;
+      case 38:
+        callDirection("forward");
+        break;
+      case 39:
+        callDirection("right");
+        break;
+      case 40:
+        callDirection("back");
+        break;
+    }
+  }
+
+  function keyUp() {
+    callDirection("stop");
+  }
+
+  function initializeVoiceControl() {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    const SpeechGrammarList =
+      window.SpeechGrammarList || window.webkitSpeechGrammarList;
+    const SpeechRecognitionEvent =
+      window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
+    const grammar =
+      "#JSGF V1.0; grammar colors; public <color> = start | stop | back | forward | left | right ";
+    const recognition = new SpeechRecognition();
+    const speechRecognitionList = new SpeechGrammarList();
+
+    speechRecognitionList.addFromString(grammar, 1);
+    recognition.grammars = speechRecognitionList;
+    recognition.lang = "en-US";
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    voiceControl.addEventListener("click", () => {
+      recognition.start();
     });
 
-    // Joystick
-    $(leftButton)
-        .on('mouseup touchend', function() {
-            callDirection('stop');
-            $(this).removeClass(pressed);
-        })
-        .on('mousedown touchstart', function() {
-            callDirection('left');
-            $(this).addClass(pressed);
-        });
+    recognition.onresult = (event) => {
+      const voiceCommand = event.results[0][0].transcript;
+      const commands = ["back", "forward", "left", "right", "start", "stop"];
 
-    $(rightButton)
-        .on('mouseup touchend', function() {
-            callDirection('stop');
-            $(this).removeClass(pressed);
-        })
-        .on('mousedown touchstart', function() {
-            callDirection('right');
-            $(this).addClass(pressed);
-        });
-
-    $(forwardButton)
-        .on('mouseup touchend', function() {
-            callDirection('stop');
-            $(this).removeClass(pressed);
-        })
-        .on('mousedown touchstart', function() {
-            callDirection('forward');
-            $(this).addClass(pressed);
-        });
-
-    $(backButton)
-        .on('mouseup touchend', function() {
-            callDirection('stop');
-            $(this).removeClass(pressed);
-        })
-        .on('mousedown touchstart', function() {
-            callDirection('back');
-            $(this).addClass(pressed);
-        });
-
-    // Keyboard commands
-    $(document).keydown(keyDown).keyup(keyUp);
-
-    function keyDown(e) {
-
-        if(e.keyCode === 37) {
-            callDirection('left')
-        } else if(e.keyCode === 38) {
-            callDirection('forward')
-        } else if(e.keyCode === 39) {
-            callDirection('right')
-        } else if(e.keyCode === 40) {
-            callDirection('back')
+      commands.forEach((command) => {
+        if (command === voiceCommand) {
+          callDirection(command);
         }
-    }
-
-    function keyUp() {
-        callDirection('stop');
-    }
-
-    function initializeVoiceControl() {
-        var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-        var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-        var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
-
-        var grammar = '#JSGF V1.0; grammar colors; public <color> = start | stop | back | forward | left | right ';
-        var recognition = new SpeechRecognition();
-        var speechRecognitionList = new SpeechGrammarList();
-
-        speechRecognitionList.addFromString(grammar, 1);
-        recognition.grammars = speechRecognitionList;
-
-        recognition.lang = 'en-US';
-        recognition.interimResults = false;
-        recognition.maxAlternatives = 1;
-
-        $(voiceControl).click(function () {
-            recognition.start();
-        });
-
-        recognition.onresult = function(event) {
-            var voiceCommand = event.results[0][0].transcript;
-
-            var commands = [
-                'back',
-                'forward',
-                'left',
-                'right',
-                'start',
-                'stop'
-            ];
-
-            commands.forEach(function(command) {
-                if(command === voiceCommand) {
-                    callDirection(command);
-                }
-            });
-        };
-
-        recognition.onspeechend = function() {
-            recognition.stop();
-        };
-    }
-
-    var engineActive = function(command) {
-        var car = { key: command };
-
-        $.ajax({
-            type: "POST",
-            url: '/start',
-            data: JSON.stringify(car),
-            contentType: "application/json",
-            success: function(res) {
-                console.log('Server res: ', res);
-            }
-        });
+      });
     };
 
-    var callDirection = function(command) {
-        var car = { key: command };
-
-        if(engineStart) {
-            $.ajax({
-                type: "POST",
-                url: '/',
-                data: JSON.stringify(car),
-                contentType: "application/json",
-                success: function(res) {
-                    console.log('Server res: ', res);
-                }
-            });
-        }
+    recognition.onspeechend = () => {
+      recognition.stop();
     };
+  }
+
+  function engineActive(command) {
+    const car = { key: command };
+
+    fetch("/start", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(car),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("Server res: ", data))
+      .catch((error) => console.error("Error:", error));
+  }
+
+  function callDirection(command) {
+    if (engineStart) {
+      const car = { key: command };
+
+      fetch("/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(car),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log("Server res: ", data))
+        .catch((error) => console.error("Error:", error));
+    }
+  }
 });
