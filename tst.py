86375acc7a94aaa83fd01a7cfb00a3737dddb7d3
Haldun <atarhaldun@gmail.com>
2025-10-24 10:37:01 +0200

    fix

--- tst.py
+++ tst.py
@@ -3,16 +3,16 @@ import time
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering
-GPIO.setup(4, GPIO.OUT) # Set pin 7 as output
+GPIO.setup(4, GPIO.OUT) # Set pin 4 as output
 
 try:
     while True:
-        GPIO.output(7, GPIO.HIGH)  # Turn ON
-        print("GPIO 7 ON")
+        GPIO.output(4, GPIO.HIGH)  # Turn ON
+        print("GPIO 4 ON")
         time.sleep(5)              # Wait 5 seconds
         
-        GPIO.output(7, GPIO.LOW)   # Turn OFF
-        print("GPIO 7 OFF")
+        GPIO.output(4, GPIO.LOW)   # Turn OFF
+        print("GPIO 4 OFF")
         time.sleep(5)              # Wait 5 seconds
 
 except KeyboardInterrupt:
